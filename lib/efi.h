5c84b1e69f67fc1e9fea2db3944a74da92b40ee4
Zixuan Wang <zixuanwang@google.com>
2021-07-26 22:59:11 +0000

    x86 UEFI: Set up memory allocator
    From: Zixuan Wang <zixuanwang@google.com>
    
    KVM-Unit-Tests library implements a memory allocator which requires
    two arguments to set up (See `lib/alloc_phys.c:phys_alloc_init()` for
    more details):
       1. A base (start) physical address
       2. Size of available memory for allocation
    
    To get this memory info, we scan all the memory regions returned by
    `LibMemoryMap()`, find out the largest free memory region and use it for
    memory allocation.
    
    After retrieving this memory info, we call `ExitBootServices` so that
    KVM-Unit-Tests has full control of the machine, and UEFI will not touch
    the memory after this point.
    
    Starting from this commit, `x86/hypercall.c` test case can run in UEFI
    and generates the same output as in Seabios.
    
    Co-developed-by: Varad Gautam <varad.gautam@suse.com>
    Signed-off-by: Varad Gautam <varad.gautam@suse.com>
    Signed-off-by: Zixuan Wang <zixuanwang@google.com>

--- lib/efi.h
+++ lib/efi.h
@@ -11,6 +11,16 @@
 #include "linux/efi.h"
 #include <elf.h>
 
+/*
+ * efi_bootinfo_t: stores EFI-related machine info retrieved before exiting EFI
+ * boot services, and is then used by setup_efi(). setup_efi() cannot retrieve
+ * this info as it is called after ExitBootServices and thus some EFI resources
+ * and functions are not available.
+ */
+typedef struct {
+	struct efi_boot_memmap mem_map;
+} efi_bootinfo_t;
+
 efi_status_t _relocate(long ldbase, Elf64_Dyn *dyn, efi_handle_t handle,
 		       efi_system_table_t *sys_tab);
 efi_status_t efi_get_memory_map(struct efi_boot_memmap *map);
