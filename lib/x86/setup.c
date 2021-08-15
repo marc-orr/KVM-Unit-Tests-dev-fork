bca4f5316236a27f2ac68d893c28b7554258aa2d
Zixuan Wang <zixuanwang@google.com>
2021-08-15 23:01:01 +0000

    Backport x86 UEFI: Set up memory allocator
    This commit fixes two issues in the previously merged commit "x86 UEFI:
    Set up memory allocator":
    
    1. Memory region check:
    
    The previous version of setup_pre_boot_memory() checkes free_mem_start
    and fails if it equals to 0. But in case the largest continuous memory
    region starts from physical address 0 with a non-zero size, this check
    fails, although the memory region is valid to use.
    
    This commit fixes it by removing the free_mem_start check.
    
    2. Buffer definition:
    
    This commit backports a bug fix from RFC V1 patch set [1] that fixes a
    type definition bug. In summary, we cannot define
    'lib/x86/setup.c:setup_pre_boot_memory():buffer' as
    'EFI_MEMORY_DESCRIPTOR *', because each buffer entries contains padding
    data that excedes the size of EFI_MEMORY_DESCRIPTOR. So we have to
    define it as 'char *', canculate buffer entry offset and then convert it
    to EFI_MEMORY_DESCRIPTOR.
    
    [1] https://lore.kernel.org/kvm/20210818000905.1111226-1-zixuanwang@google.com/T/#m2dcb430d7e950f6ad791745f1d74a1004ccc8555
    
    Change-Id: I5ef0e693846b811ee5100bca5674694b2aed485c

--- lib/x86/setup.c
+++ lib/x86/setup.c
@@ -136,15 +136,22 @@ void setup_efi_bootinfo(efi_bootinfo_t *efi_bootinfo)
 	efi_bootinfo->free_mem_start = 0;
 }
 
-EFI_STATUS setup_pre_boot_memory(UINTN *mapkey, efi_bootinfo_t *efi_bootinfo)
+static EFI_STATUS setup_pre_boot_memory(UINTN *mapkey, efi_bootinfo_t *efi_bootinfo)
 {
 	UINTN total_entries, desc_size;
 	UINT32 desc_version;
-	EFI_MEMORY_DESCRIPTOR *buffer;
+	char *buffer;
 	int i;
 	UINT64 free_mem_total_pages = 0;
 
-	buffer = LibMemoryMap(&total_entries, mapkey, &desc_size, &desc_version);
+	/* Although buffer entries are later converted to EFI_MEMORY_DESCRIPTOR,
+	 * we cannot simply define buffer as 'EFI_MEMORY_DESCRIPTOR *buffer'.
+	 * Because the actual buffer entry size 'desc_size' is bigger than
+	 * 'sizeof(EFI_MEMORY_DESCRIPTOR)', i.e. there are padding data after
+	 * each EFI_MEMORY_DESCRIPTOR. So defining 'EFI_MEMORY_DESCRIPTOR
+	 * *buffer' leads to wrong buffer entries fetched.
+	 */
+	buffer = (char *)LibMemoryMap(&total_entries, mapkey, &desc_size, &desc_version);
 	if (desc_version != 1) {
 		return EFI_INCOMPATIBLE_VERSION;
 	}
@@ -155,17 +162,19 @@ EFI_STATUS setup_pre_boot_memory(UINTN *mapkey, efi_bootinfo_t *efi_bootinfo)
 	 * allocator, so that the memory allocator can work in the largest free
 	 * continuous memory region.
 	 */
-	for (i = 0; i < total_entries; i ++) {
-		if (buffer[i].Type == EfiConventionalMemory) {
-			if (free_mem_total_pages < buffer[i].NumberOfPages) {
-				free_mem_total_pages = buffer[i].NumberOfPages;
+	for (i = 0; i < total_entries * desc_size; i += desc_size) {
+		EFI_MEMORY_DESCRIPTOR *d = (EFI_MEMORY_DESCRIPTOR *)&buffer[i];
+
+		if (d->Type == EfiConventionalMemory) {
+			if (free_mem_total_pages < d->NumberOfPages) {
+				free_mem_total_pages = d->NumberOfPages;
 				efi_bootinfo->free_mem_size = free_mem_total_pages * EFI_PAGE_SIZE;
-				efi_bootinfo->free_mem_start = buffer[i].PhysicalStart;
+				efi_bootinfo->free_mem_start = d->PhysicalStart;
 			}
 		}
 	}
 
-	if ((efi_bootinfo->free_mem_size == 0) || (efi_bootinfo->free_mem_start == 0)) {
+	if (efi_bootinfo->free_mem_size == 0) {
 		return EFI_OUT_OF_RESOURCES;
 	}
 
@@ -196,7 +205,7 @@ EFI_STATUS setup_efi_pre_boot(UINTN *mapkey, efi_bootinfo_t *efi_bootinfo)
 	return EFI_SUCCESS;
 }
 
-void setup_gdt_tss()
+static void setup_gdt_tss(void)
 {
 	gdt_entry_t *tss_lo, *tss_hi;
 	tss64_t *curr_tss;
